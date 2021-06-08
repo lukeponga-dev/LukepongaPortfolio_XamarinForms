63d6551edf9531bf9c79317050d5b85ff388fb35
Luke Devops <developmentdesignsltd@gmail.com>
2021-06-09 02:30:55 +1200

    Add project files.

---
+++ LukePonga_Portfolio/LukePonga_Portfolio/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LukePonga_Portfolio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var browser = new WebView();
+            browser.Source = "https://www.lukeponga.website/";
+            Content = browser;
+        }
+    }
+}
